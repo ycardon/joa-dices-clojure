16675214672820d7a9c31256ab579443ea80ac64
Yann Cardon <ycardon@gmail.com>
2020-10-24 00:31:19 +0200

    cleanup

--- src/joa_dices_clojure/core.clj
+++ src/joa_dices_clojure/core.clj
@@ -1,6 +1,16 @@
 (ns joa-dices-clojure.core
   (:require [joa-dices-clojure.dice :refer :all]))
 
+; apply counter shields on the attack and remove unrelevant faces from the attack
+(defn apply-counter [attack counter]
+  (let [[result _] (->> [attack (count-face :shield counter)]
+                        (cancel :kill)
+                        (cancel :disrupt)
+                        (cancel :push))]
+    (->> result
+         (filter (partial not= :shield))
+         (filter (partial not= :blank)))))
+
 ; cancel roll faces by an amount of shield count
 (defn cancel [face [roll shield-count]]
   (defn f [[r s] cur]
@@ -14,25 +24,27 @@
 (defn count-face [face roll]
   (count (filter (partial = face) roll)))
 
-; apply defence shields on the attack and remove unrelevant faces from the attack
-(defn apply-defense [attack defense]
-  (let [[result _] (->> [attack (count-face :shield defense)]
-                        (cancel :kill)
-                        (cancel :disrupt)
-                        (cancel :push))]
-    (->> result
-         (filter (partial not= :shield))
-         (filter (partial not= :blank)))))
-
-; frequency of the faces in a roll
-(defn frequency [roll]
+; frequency of the faces in a roll (just for fun, already present in the std lib)
+(defn frequencies' [roll]
   (apply merge-with + (for [face roll] {face 1})))
+
+; compute the result of a fight
+(defn fight [attack-dices counter-dices counter?]
+  (let [attack  (roll-dices attack-dices)
+        counter (roll-dices counter-dices)]
+    (if counter?
+      {:attack (frequencies attack)
+       :counter (frequencies counter)
+       :result (frequencies (apply-counter attack counter))}
+      (frequencies attack))))
 
 ; tests
 (rolln 1 black-dice)
 (cancel :push (cancel :kill [(rolln 3 black-dice) 3]))
 (count-face :kill (rolln 100 red-dice))
-(apply-defense (rolln 10 red-dice) (rolln 10 black-dice))
+(apply-counter (rolln 10 red-dice) (rolln 10 black-dice))
 (roll-dices [[1 black-dice] [2 red-dice]])
-(apply-defense [:kill :kill :push :blank] [:shield :shield])
-(frequency [:kill :kill :push :blank])
+(apply-counter [:kill :kill :push :blank] [:shield :shield])
+(frequencies' (apply-counter (rolln 100 red-dice) (rolln 80 black-dice)))
+(frequencies [:kill :kill :push :blank])
+(fight [[100 red-dice]] [[80 black-dice]] true)
